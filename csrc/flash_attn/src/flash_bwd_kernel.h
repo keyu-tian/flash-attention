ae3daa9939a85ead35b4562946cf0c2b062f4fda
keyu-tian <k_code@163.com>
2024-05-29 19:11:17 +0800

    [upd] only upd comments

--- csrc/flash_attn/src/flash_bwd_kernel.h
+++ csrc/flash_attn/src/flash_bwd_kernel.h
@@ -502,9 +502,7 @@ inline __device__ void compute_dq_dk_dv_1colblock(const Params &params, const in
                 || (!Is_even_MN && (n_block + 1) * kBlockN >= binfo.actual_seqlen_k)) {
                 flash::apply_mask_causal(scores, n_block * kBlockN + (tidx / 32 / AtomLayoutMS) * MMA_N_SdP * 16,
                                          binfo.actual_seqlen_k, m_block * kBlockM + get<0>(taccScS_row(0)),
-                                         binfo.actual_seqlen_q,
-                                         // binfo.actual_seqlen_k, m_block * kBlockM + (tidx / 32) % AtomLayoutMS * 16 + (tidx % 32) / 4,
-                                         AtomLayoutMS * 16,
+                                         binfo.actual_seqlen_q, AtomLayoutMS * 16,
                                          params.VAR_visible_kvlen);
             }
         } else if (Is_local) {              // not causal, not VAR
